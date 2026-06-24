6afebf20564863e8cb26d3e3f49580f03fb1fc1e
Belleza Admin <admin@beautyapp.com>
2026-06-24 03:30:00 -0500

    Fix avatar load crash on client and provider profiles

--- backend/public/flutter_bootstrap.js
+++ backend/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1476793086" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "925384506" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
