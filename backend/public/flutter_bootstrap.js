e4787e029b7cb45cc1b33513628f5f65a6eb572c
Belleza Admin <admin@beautyapp.com>
2026-06-28 20:48:47 -0500

    fix: remueve boton de chats del dock inferior para coincidir con app movil

--- backend/public/flutter_bootstrap.js
+++ backend/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2094270238" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2678061019" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
